6c5dcdbbbc0ef6750f5fb0926a0c236c29dbcf22
Amir Rezvani <a.h.rezvani@gmail.com>
2019-05-31 19:49:47 -0700

    bad build

---
+++ CCGradient/CCGradientView/CCGradientView.swift
@@ -0,0 +1,127 @@
+//
+//  CCGradientView.swift
+//  CCGradient
+//
+//  Created by Amir REZVANI on 5/30/19.
+//  Copyright © 2019 Amir REZVANI. All rights reserved.
+//
+//MIT License
+//
+//Copyright (c) 2019 Amir Rezvani
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+import UIKit
+
+// MARK: - GradientType enum
+
+public enum CCGradientType {
+    case axial
+    case radial
+    case conic
+    
+    fileprivate func toCAGradientLayerType() -> CAGradientLayerType {
+        switch self {
+        case .axial:
+            return .axial
+        case .radial:
+            return .radial
+        case .conic:
+            return .conic
+        }
+    }
+}
+
+// MARK: - CCGradient configuration
+
+public protocol CCGradientBasicConfiguration: class {
+    func colors() -> [UIColor]
+}
+
+public protocol CCGradientAdvancedConfiguration: class {
+    func gradientType() -> CCGradientType
+    func points() -> [CGPoint]
+}
+
+extension CCGradientAdvancedConfiguration {
+    func gradientType() -> CCGradientType {
+        return .axial
+    }
+    
+    func points() -> [CGPoint] {
+        return [CGPoint(x: 0.5, y: 0.5),
+                CGPoint(x: 1, y: 1)]
+    }
+}
+
+// MARK: - CCGradientView
+
+public class CCGradientView: UIView {
+    
+    // MARK: - private properties
+    
+    private var gradientLayer: CAGradientLayer!
+    
+    // MARK: - public properties
+    
+    public weak var basicConfiguration: CCGradientBasicConfiguration?
+    public weak var advancedConfigurations: CCGradientAdvancedConfiguration?
+    
+    // MARK: - [initializer]s
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addGradientLayer()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        addGradientLayer()
+    }
+    
+    // MARK: - lifecycle
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        gradientLayer.frame = bounds
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let colors = (basicConfiguration?.colors() ?? [UIColor.red, UIColor.blue]).map{ $0.cgColor }
+        gradientLayer.colors = colors
+        gradientLayer.type = (advancedConfigurations?.gradientType() ?? .axial).toCAGradientLayerType()
+        if let points = advancedConfigurations?.points() {
+            gradientLayer.startPoint = points[0]
+            gradientLayer.endPoint = points[1]
+        }
+        
+    }
+    
+    // MARK: - private functions
+    
+    func addGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        layer.addSublayer(gradientLayer)
+    }
+}
--- CCGradient/ViewController.swift
+++ CCGradient/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gradientView: CCGradientView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        gradientView.basicConfiguration = self
+        gradientView.advancedConfigurations = self
     }
-
 
 }
 
+extension ViewController: CCGradientBasicConfiguration, CCGradientAdvancedConfiguration {
+    func colors() -> [UIColor] {
+        return [
+            UIColor(red: 247.0/255.0, green: 151.0/255.0, blue: 30.0/255.0, alpha: 1.0),
+            UIColor(red: 255.0/255.0, green: 210.0/255.0, blue: 0/255.0, alpha: 1.0)
+        ]
+    }
+    
+    func points() -> [CGPoint] {
+        return [CGPoint(x: 0, y: 0.5), CGPoint(x: 1, y: 0.5)]
+    }
+}
